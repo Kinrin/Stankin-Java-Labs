4994535269d6dc783261051157eac23be813a2cd
Kinrin <kinrin1@yandex.ru>
2019-01-10 16:16:59 +0300

    fix blocking operations

--- lab4/src/main/java/Main.java
+++ lab4/src/main/java/Main.java
@@ -1,24 +1,17 @@
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
 import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.core.streams.Pump;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 
 public class Main extends AbstractVerticle {
-    public String getFileContent ( FileInputStream fis ) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        Reader r = new InputStreamReader(fis, "UTF-8");
-        char[] buf = new char[1024];
-        int amt = r.read(buf);
-        while(amt > 0) {
-            sb.append(buf, 0, amt);
-            amt = r.read(buf);
-        }
-        return sb.toString();
-    }
 
     @Override
     public void start()  {
@@ -26,40 +19,44 @@ public class Main extends AbstractVerticle {
         vertx.createHttpServer().requestHandler(req -> {
 
             req.pause();
-            try {
-                FileInputStream fil = new FileInputStream("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\checksumoffiles.txt");
-                String lifi  = getFileContent(fil);
 
-                String filename = req.getHeader("content-name");
+                Path path = Paths.get("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\checksumoffiles.txt");
 
-                String chk = req.getHeader("content-checksum")+ " ";
-                boolean isFound = lifi.indexOf(chk) !=-1? true: false;
-                if(!isFound) {
-                    while (new File("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\" + filename).exists()){
-                        filename = "new_" + filename ;
+                try {
+                    String lifi = "";
+                    List<String> tmp = Files.readAllLines(path);
+                    for (String s : tmp)
+                    {
+                        lifi += s + " ";
                     }
-                    vertx.fileSystem().open("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\" + filename, new OpenOptions(), ares -> {
-                        AsyncFile file = ares.result();
-                        Pump pump = Pump.pump(req, file);
-                        req.endHandler(v1 -> file.close(v2 -> {
-                            req.response().end();
-                        }));
-                        pump.start();
-                        req.resume();
+                    String filename = req.getHeader("content-name");
+                    String chk = req.getHeader("content-checksum") + " ";
+                    boolean isFound = lifi.indexOf(chk) != -1 ? true : false;
+                    if (!isFound) {
+                        while (new File("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\" + filename).exists()) {
+                            filename = "new_" + filename;
+                        }
+                        vertx.fileSystem().open("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\" + filename, new OpenOptions(), ares -> {
+                            AsyncFile file = ares.result();
+                            Pump pump = Pump.pump(req, file);
+                            req.endHandler(v1 -> file.close(v2 -> {
+                                req.response().end();
+                            }));
+                            pump.start();
+                            req.resume();
 
-                    });
-                    FileOutputStream os = new FileOutputStream("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\checksumoffiles.txt",true);
-                    os.write(chk.getBytes(), 0, chk.length());
-                    os.close();
-                }
-                else{
-                    System.out.println("Error that file already here!");
-                    req.response().setStatusCode(500);
-                    req.response().end();
+                        });
+                        Path os = Paths.get("C:\\Users\\Михаил\\IdeaProjects\\lab4\\serverfiles\\checksumoffiles.txt");
+                        Files.write(os, chk.getBytes(), StandardOpenOption.APPEND);
 
-                }
-            }catch (IOException e ){
-                System.out.println(e.getCause());
+                    } else {
+                        System.out.println("Error that file already here!");
+                        req.response().setStatusCode(500);
+                        req.response().end();
+
+                    }
+                }  catch (IOException ex) {
+                ex.printStackTrace();
             }
         }).listen(8080);
 
@@ -69,7 +66,5 @@ public class Main extends AbstractVerticle {
         vertx.deployVerticle(new Main());
     }
 }
-
-
 
 
